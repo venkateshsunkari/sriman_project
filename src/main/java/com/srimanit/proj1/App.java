30b123e54b7970be51d72d51d9b6585da8a4270b
venky <root@ip-71-0-1-247.ap-south-1.compute.internal>
2022-02-09 05:31:54 +0000

    sriman project

---
+++ src/main/java/com/srimanit/proj1/App.java
@@ -0,0 +1,13 @@
+package com.srimanit.proj1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
